eb22fad25a0cfc71400680f202108817777f02a5
Toua Thao <touathao09@gmail.com>
2019-03-25 07:54:07 -0500

    added more interview question

---
+++ docker.js
@@ -0,0 +1,13 @@
+// Our project agentAve use Serverless application model
+//what is a docker use for
+//Docker use container.  Each container allow a developer 
+//to package up an application with all of the parts it needs, such as libraries and other dependencies, 
+//and ship it all out as one package
+//This allow or eliminate customized settings that machine might have that could differ 
+//from the machine used for writing and testing the code. 
+
+//////////////////////////////////
+// one way hashing or two hashing
+
+//puppeteer
+//manual testing and automatic testing
---
+++ top10css.js
@@ -0,0 +1,23 @@
+//top ten css question.
+//what is box model?
+//it's a model that wrap element in HTML in a box.
+//This include content,padding,boarder,margin
+//////////////////////
+//what is specificiy?
+// i.e
+//div background red
+//div background blue
+//it will pick the last one. so it will be blue.
+//but if it's div.name vs div. the div.name will have the color
+// div class=name 
+// div
+// but if it's ID. Then we will take the ID instead. i.e
+//dive class=name vs div Id=myId
+// another way around this is to force by using !important
+//////////////////////
+//how do you align a block element inside a element
+//create two div.
+//outter div and inner div.
+//both div should have width, height.
+//outter div should have a position of relative 
+//inner div should have a postion of absolute. 50% top, 50% left. transform:translate (-50%,-50%) 
